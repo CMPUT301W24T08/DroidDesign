d3a4048bf4ad3f46e885fa0f5bd8e3c25a9fbbf9
Nanribet Fwangje <fwangje@ualberta.ca>
2024-02-29 16:28:35 -0700

    Created Admin

---
+++ app/src/main/java/com/example/droiddesign/model/Admin.java
@@ -0,0 +1,16 @@
+package com.example.droiddesign.model;
+
+public class Admin extends User {
+	private String type; // Additional fields specific to Admin
+
+
+	@Override
+	public void saveToFirestore() {
+
+	}
+
+	@Override
+	public void loadFromFirestore(String userId) {
+
+	}
+}
