ab4740c442a6220fe80a6d1cf2a0844a6e1b7a93
Nanribet Fwangje <fwangje@ualberta.ca>
2024-02-29 17:23:35 -0700

    Modified LaunchScreenActivity.java to startActivity to the next page using skipbutton

--- app/src/main/java/com/example/droiddesign/view/LaunchScreenActivity.java
+++ app/src/main/java/com/example/droiddesign/view/LaunchScreenActivity.java
@@ -1,16 +1,29 @@
 package com.example.droiddesign.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.droiddesign.R;
 
 public class LaunchScreenActivity extends AppCompatActivity {
 
+    private Button skipButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+        skipButton = findViewById(R.id.skip_button);
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchScreenActivity.this, EventMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
