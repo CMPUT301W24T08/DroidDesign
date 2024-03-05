cd2cc3f4416052923304bce98447dacc6358ce45
Nanribet Fwangje <fwangje@ualberta.ca>
2024-03-05 00:24:42 -0700

    Updated AddEventActivity

--- app/src/main/java/com/example/droiddesign/view/AddEventActivity.java
+++ app/src/main/java/com/example/droiddesign/view/AddEventActivity.java
@@ -1,5 +1,6 @@
 package com.example.droiddesign.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -84,6 +85,15 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerFra
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        FloatingActionButton fabNextPage = findViewById(R.id.fab_next_page);
+        fabNextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the AddEventSecondActivity
+                startActivity(new Intent(AddEventActivity.this, AddEventSecondActivity.class));
             }
         });
     }
