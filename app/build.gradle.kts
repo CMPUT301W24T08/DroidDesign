fccbe216043c2afcda3341faad0e7854c357bd8d
chris-vanderveen <cjvander@ualberta.ca>
2024-03-06 13:36:07 -0700

    start working on testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    implementation("com.google.zxing:core:3.4.1")
+    implementation("com.google.zxing:core:3.5.3")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.squareup.picasso:picasso:2.71828")
 
