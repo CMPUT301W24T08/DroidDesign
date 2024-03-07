8e816ccbd463923f2cd13164ac5aedba1c8bd0ec
chris-vanderveen <cjvander@ualberta.ca>
2024-03-06 21:57:49 -0700

    fixed uml issues

--- app/src/main/java/com/example/droiddesign/model/Admin.java
+++ app/src/main/java/com/example/droiddesign/model/Admin.java
@@ -1,11 +1,6 @@
 package com.example.droiddesign.model;
 
 import java.util.HashMap;
-<<<<<<< HEAD
-
-public class Admin {
-=======
-import java.util.Map;
 
 public class Admin extends User {
 	private String type; // Additional fields specific to Admin
@@ -13,14 +8,13 @@ public class Admin extends User {
 	public Admin(String userId, String role) {
 		super(userId, role);
 	}
+
 	@Override
 	public HashMap<String, Object> toMap() {
 		HashMap<String, Object> map = new HashMap<>();
 		map.put("userId", userId);
-		map.put("role",role);
+		map.put("role", role);
 		return map;
 	}
-
->>>>>>> 736f5d5205a9ea65fcca4fd46b2600799e177ae0
-
 }
+
--- app/src/main/java/com/example/droiddesign/view/BasicLoginFragment.java
+++ app/src/main/java/com/example/droiddesign/view/BasicLoginFragment.java
@@ -56,6 +56,8 @@ public class BasicLoginFragment extends DialogFragment {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             FirebaseUser user = mAuth.getCurrentUser();
+                            assert user != null;
+                            User firestoreuser = new Organizer(user.getUid());
                         }
                     }
                 });
