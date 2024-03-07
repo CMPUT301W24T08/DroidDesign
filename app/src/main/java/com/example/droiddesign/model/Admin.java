884d05f5db70dc3baf552a78a7a48c2c04e4fc7d
FardilIqbal <iqbalfardil@gmail.com>
2024-03-06 21:34:20 -0700

    Simplified admin

--- app/src/main/java/com/example/droiddesign/model/Admin.java
+++ app/src/main/java/com/example/droiddesign/model/Admin.java
@@ -1,16 +1,21 @@
 package com.example.droiddesign.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Admin extends User {
 	private String type; // Additional fields specific to Admin
 
-
+	public Admin(String userId, String role) {
+		super(userId, role);
+	}
 	@Override
-	public void saveToFirestore() {
-
+	public HashMap<String, Object> toMap() {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("role",role);
+		return map;
 	}
 
-	@Override
-	public void loadFromFirestore(String userId) {
 
-	}
 }
