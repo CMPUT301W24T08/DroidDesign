29070238106ef2a984d69c7f65be8251b64a5e9d
Nanribet Fwangje <fwangje@ualberta.ca>
2024-03-14 20:59:45 -0600

    Modified new user class

--- app/src/main/java/com/example/droiddesign/model/User.java
+++ app/src/main/java/com/example/droiddesign/model/User.java
@@ -7,36 +7,103 @@ import java.util.HashMap;
  * Represents a generic user in the system with common attributes like user ID and role.
  * This class is abstract and intended to be subclassed by specific types of users.
  */
-public abstract class User {
+public class User {
 	/**
-	 * Unique identifier for the user.
+	 * The unique id of the user.
 	 */
-	protected String userId;
+	private String userId;
+	/**
+	 * The role user takes on.
+	 */
+	private String role;
+	/**
+	 * The profile name of the user.
+	 */
+	private String profileName;
 
 	/**
-	 * The primary role of the user in the system.
+	 * The email address of the user.
 	 */
-	protected String role;
+	private String email;
 
 	/**
-	 * A list of roles associated with the user. Though not initialized or used in the given code,
-	 * this implies that users might have multiple roles.
+	 * The phone number of the user.
 	 */
-	protected ArrayList<String> rolesList;
+	private String phone;
 
 	/**
-	 * Constructs a User instance with the specified user ID and role.
+	 * The URL or path to the profile picture of the user.
+	 */
+	private String profilePic;
+
+	/**
+	 * A flag indicating whether geolocation features are enabled for the user.
+	 */
+	private boolean geolocation;
+
+	/**
+	 * A list of event IDs that this user is associated with.
+	 */
+	private ArrayList<String> signedEventsList;
+	/**
+	 * A list of event IDs that this user manage.
+	 */
+	private ArrayList<String> managedEventsList;
+
+	/**
+	 * Constructs an empty User instance with default values.
+	 */
+	public User() {
+		this.userId = null;
+		this.role = null;
+		this.profileName = "";
+		this.email = "";
+		this.phone = "";
+		this.profilePic = "";
+		this.geolocation = false;
+		this.signedEventsList = new ArrayList<>();
+		this.managedEventsList = new ArrayList<>();
+	}
+
+
+	/**
+	 * Constructs an User instance with the specified user ID and role, initializing user-specific fields to default values.
 	 *
 	 * @param userId The unique identifier for the user.
-	 * @param role The role of the user within the system.
+	 * @param role   The role of the user within the system.
 	 */
 	public User(String userId, String role) {
 		this.userId = userId;
 		this.role = role;
+		this.profileName = "";
+		this.email = "";
+		this.phone = "";
+		this.profilePic = "";
+		this.geolocation = false;
+		this.signedEventsList = new ArrayList<>();
+		this.managedEventsList = new ArrayList<>();
 	}
 
 	/**
-	 * Retrieves the user ID.
+	 * Converts the user's data to a map structure for easy storage and retrieval.
+	 *
+	 * @return A map of user attributes to their respective values.
+	 */
+	public HashMap<String, Object> toMap() {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("role", role);
+		map.put("email", email);
+		map.put("phone", phone);
+		map.put("profileName", profileName);
+		map.put("profilePic", profilePic);
+		map.put("signedEventsList", signedEventsList);
+		map.put("managedEventsList", managedEventsList);
+		return map;
+	}
+
+	/**
+	 * Gets the user ID.
 	 *
 	 * @return The user ID.
 	 */
@@ -45,7 +112,16 @@ public abstract class User {
 	}
 
 	/**
-	 * Retrieves the user's role.
+	 * Sets the user ID.
+	 *
+	 * @param userId The user ID to set.
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * Gets the role of the user.
 	 *
 	 * @return The role of the user.
 	 */
@@ -54,11 +130,149 @@ public abstract class User {
 	}
 
 	/**
-	 * Converts the user object into a map structure suitable for database storage.
-	 * This method is abstract and must be implemented by subclasses.
+	 * Sets the role of the user.
 	 *
-	 * @return A map representation of the user object.
+	 * @param role The role to set for the user.
 	 */
-	public abstract HashMap<String, Object> toMap();
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	/**
+	 * Sets the profile name of the user.
+	 *
+	 * @param profileName The new profile name to be set.
+	 */
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	/**
+	 * Gets the profile name of the user.
+	 *
+	 * @return The profile name of the user.
+	 */
+	public String getProfileName() {
+		return profileName;
+	}
+
+	/**
+	 * Sets the email address of the user.
+	 *
+	 * @param email The new email address to be set.
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Gets the email address of the user.
+	 *
+	 * @return The email address of the user.
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	 * Sets the phone number of the user.
+	 *
+	 * @param phone The new phone number to be set.
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * Gets the phone number of the user.
+	 *
+	 * @return The phone number of the user.
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * Sets the URL or path to the profile picture of the user.
+	 *
+	 * @param profilePic The new URL or path to the profile picture.
+	 */
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+
+	/**
+	 * Sets the geolocation availability for the user.
+	 *
+	 * @param geolocation The new geolocation status to be set.
+	 */
+	public void setGeolocation(boolean geolocation) {
+		this.geolocation = geolocation;
+	}
+
+	/**
+	 * Checks if geolocation is enabled for the user.
+	 *
+	 * @return true if geolocation is enabled, false otherwise.
+	 */
+	public boolean getGeolocation() {
+		return geolocation;
+	}
+
+	/**
+	 * Adds an event to the user's list of events by event ID.
+	 *
+	 * @param event The event to be added to the user's list.
+	 */
+	public void addSignedEvent(Event event) {
+		signedEventsList.add(event.getEventId());
+	}
+
+
+	/**
+	 * Gets the profile picture of the user.
+	 *
+	 * @return The profile picture of the user.
+	 */
+	public String getProfilePic() {
+		return this.profilePic;
+	}
+
+	/**
+	 * Gets the list of events the user has signed up for.
+	 *
+	 * @return The list of events the user has signed up for.
+	 */
+	public ArrayList<String> getSignedEventsList() {
+		return this.signedEventsList;
+	}
+
+	/**
+	 * Sets the list of events the user has signed up for.
+	 *
+	 * @param signedEventsList The list of events the user has signed up for.
+	 */
+	public void setSignedEventsList(ArrayList<String> signedEventsList) {
+		this.signedEventsList = signedEventsList;
+	}
+
+	/**
+	 * Gets the list of events the user manages.
+	 *
+	 * @return The list of events the user manages.
+	 */
+	public ArrayList<String> getManagedEventsList() {
+		return managedEventsList;
+	}
+
+	/**
+	 * Sets the list of events the user manages.
+	 *
+	 * @param managedEventsList The list of events the user manages.
+	 */
+	public void setManagedEventsList(ArrayList<String> managedEventsList) {
+		this.managedEventsList = managedEventsList;
+	}
 }
 
