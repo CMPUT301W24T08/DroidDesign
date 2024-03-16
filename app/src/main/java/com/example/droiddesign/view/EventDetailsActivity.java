f311db16cc33e64bc2e62a1e7feee8a6f8be486e
Nanribet Fwangje <fwangje@ualberta.ca>
2024-03-16 09:21:08 -0600

    Created new navigation for menu at event management by org

--- app/src/main/java/com/example/droiddesign/view/EventDetailsActivity.java
+++ app/src/main/java/com/example/droiddesign/view/EventDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.droiddesign.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -12,9 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.droiddesign.R;
+import com.example.droiddesign.model.Event;
 import com.example.droiddesign.model.SharedPreferenceHelper;
 import com.example.droiddesign.model.User;
-import com.example.droiddesign.model.Event;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -95,6 +96,7 @@ public class EventDetailsActivity extends AppCompatActivity {
 		signUpButton.setOnClickListener(v -> signUpForEvent());
 
 		ImageButton menuButton = findViewById(R.id.button_menu);
+		menuButton.setOnClickListener(v -> toggleNavigationMenu());
 		navigationMenu = findViewById(R.id.navigation_menu);
 		navigationMenu.getMenu().clear();
 
@@ -147,6 +149,16 @@ public class EventDetailsActivity extends AppCompatActivity {
 
 			return true;
 		});
+	}
+	/**
+	 * Toggles the visibility of the navigation menu.
+	 */
+	private void toggleNavigationMenu() {
+		if (navigationMenu.getVisibility() == View.VISIBLE) {
+			navigationMenu.setVisibility(View.GONE);
+		} else {
+			navigationMenu.setVisibility(View.VISIBLE);
+		}
 	}
 
 	/**
