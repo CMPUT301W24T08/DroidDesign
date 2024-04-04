23c6a167d690b14338c870d2675637626b4d1bc2
FardilIqbal <iqbalfardil@gmail.com>
2024-04-04 16:03:44 -0600

    Some UI test cases, ones more reliant on firebase need further testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("androidx.activity:activity:1.8.2")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
@@ -79,7 +80,7 @@ dependencies {
 
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:3.11.2")
+    androidTestImplementation("org.mockito:mockito-core:3.11.2")
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1") // or a later version
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1") // or a later version
